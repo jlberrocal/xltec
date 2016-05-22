3e02db00e6aa9d8bead8724bfddc2b71f4df767a
Jose Berrocal <jlberrocal@users.noreply.github.com>
2016-05-21 19:34:37 -0600

    Update params.js

--- config/params.js
+++ config/params.js
@@ -4,6 +4,6 @@
 
 module.exports = {
     mongoUrl: "mongodb://admin:admin$1234@ds023078.mlab.com:23078/xltec",
-    port: 3000,
+    port: 80,
     jwt: 'authorization token, change it if you consider this risky'
 };
