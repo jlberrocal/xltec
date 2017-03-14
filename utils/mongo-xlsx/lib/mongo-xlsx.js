b13e168703a55058fc758ce48243180f9c82136a
Jose Berrocal <jose.berrocalobando@gmail.com>
2017-03-13 22:01:02 -0600

    try to fix nan

--- utils/mongo-xlsx/lib/mongo-xlsx.js
+++ utils/mongo-xlsx/lib/mongo-xlsx.js
@@ -231,7 +231,7 @@ exports.mongoData2XlsxData = function(mongoData, mongoModel) {
                 }
             }
             if(parse.type === 'number') {
-                if(!aCell)
+                if(/(object)|(null)|(undefined)/g.test(typeof aCell))
                     excelRow.push('');
                 else
                     excelRow.push(parseInt(aCell))
