5b63816bb8d0f23e4f963217e3162c875d85f108
Jose Berrocal <jose.berrocalobando@gmail.com>
2017-03-13 21:11:54 -0600

    trying to fix NaN cell

--- utils/node-xlsx/lib/helpers.js
+++ utils/node-xlsx/lib/helpers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 exports.isString = exports.isNumber = exports.isBoolean = exports.buildSheetFromMatrix = undefined;
 
@@ -12,61 +12,61 @@ var _xlsx2 = _interopRequireDefault(_xlsx);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var isBoolean = function isBoolean(maybeBoolean) {
-  return typeof maybeBoolean === 'boolean';
+    return typeof maybeBoolean === 'boolean';
 };
 var isNumber = function isNumber(maybeNumber) {
-  return typeof maybeNumber === 'number';
+    return typeof maybeNumber === 'number';
 };
 var isString = function isString(maybeString) {
-  return typeof maybeString === 'string';
+    return typeof maybeString === 'string';
 };
 
 var originDate = new Date(Date.UTC(1899, 11, 30));
 
 var buildExcelDate = function buildExcelDate(value, is1904) {
-  var epoch = Date.parse(value + (is1904 ? 1462 : 0));
-  return (epoch - originDate) / 864e5;
+    var epoch = Date.parse(value + (is1904 ? 1462 : 0));
+    return (epoch - originDate) / 864e5;
 };
 
 var buildSheetFromMatrix = function buildSheetFromMatrix(data) {
-  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-  var workSheet = {};
-  var range = { s: { c: 1e7, r: 1e7 }, e: { c: 0, r: 0 } };
-  for (var R = 0; R !== data.length; ++R) {
-    for (var C = 0; C !== data[R].length; ++C) {
-      if (range.s.r > R) range.s.r = R;
-      if (range.s.c > C) range.s.c = C;
-      if (range.e.r < R) range.e.r = R;
-      if (range.e.c < C) range.e.c = C;
-      if (data[R][C] === null) {
-        continue; // eslint-disable-line
-      }
-      var cell = { v: data[R][C] };
-      var cellRef = _xlsx2.default.utils.encode_cell({ c: C, r: R });
-      if (isNumber(cell.v)) {
-        cell.t = 'n';
-      } else if (isBoolean(cell.v)) {
-        cell.t = 'b';
-      } else if (cell.v instanceof Date) {
-        cell.t = 'd';
-        cell.v = cell.v.toISOString();
-      } else {
-        cell.t = 's';
-      }
-      workSheet[cellRef] = cell;
+    var workSheet = {};
+    var range = { s: { c: 1e7, r: 1e7 }, e: { c: 0, r: 0 } };
+    for (var R = 0; R !== data.length; ++R) {
+        for (var C = 0; C !== data[R].length; ++C) {
+            if (range.s.r > R) range.s.r = R;
+            if (range.s.c > C) range.s.c = C;
+            if (range.e.r < R) range.e.r = R;
+            if (range.e.c < C) range.e.c = C;
+            if (data[R][C] === null) {
+                continue; // eslint-disable-line
+            }
+            var cell = { v: data[R][C] };
+            var cellRef = _xlsx2.default.utils.encode_cell({ c: C, r: R });
+            if (!isNaN(cell.v) && isNumber(cell.v)) {
+                cell.t = 'n';
+            } else if (isBoolean(cell.v)) {
+                cell.t = 'b';
+            } else if (cell.v instanceof Date) {
+                cell.t = 'd';
+                cell.v = cell.v.toISOString();
+            } else {
+                cell.t = 's';
+            }
+            workSheet[cellRef] = cell;
+        }
     }
-  }
-  if (range.s.c < 1e7) {
-    workSheet['!ref'] = _xlsx2.default.utils.encode_range(range);
-  }
-  if (options['!cols']) {
-    workSheet['!cols'] = options['!cols'];
-  }
-  if (options['!merges']) {
-    workSheet['!merges'] = options['!merges'];
-  }
-  return workSheet;
+    if (range.s.c < 1e7) {
+        workSheet['!ref'] = _xlsx2.default.utils.encode_range(range);
+    }
+    if (options['!cols']) {
+        workSheet['!cols'] = options['!cols'];
+    }
+    if (options['!merges']) {
+        workSheet['!merges'] = options['!merges'];
+    }
+    return workSheet;
 };
 
 exports.buildSheetFromMatrix = buildSheetFromMatrix;
