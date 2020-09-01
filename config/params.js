b6598d6bd3cf8c741009e335e7d90e67e0acb20f
Jose Berrocal <jose.berrocalobando@gmail.com>
2020-08-31 20:36:14 -0600

    migration to mongo atlas

--- config/params.js
+++ config/params.js
@@ -1,9 +1,12 @@
 /**
  * Created by Jose on 27/04/2016.
  */
+require('dotenv').config();
 
+const {DB_USER, DB_PASS, DB_HOST, DB_NAME} = process.env;
 module.exports = {
-    mongoUrl: process.env.DB || `mongodb://admin:${encodeURIComponent('admin$1234')}@ds023078.mlab.com:23078/xltec`,
+    mongoUrl: process.env.DB,
+    mongoUrl: `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
     port: 8000,
     jwt: 'authorization sign, change it if you consider this risky'
 };
